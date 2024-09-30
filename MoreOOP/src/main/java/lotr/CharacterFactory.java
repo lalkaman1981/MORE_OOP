a3b67ad5385a8086b41e867df112a74aa4a90fa8
Oleg Shtogryn <oleg.shtogrin2005@gmail.com>
2024-09-30 16:34:50 +0300

    improved CharacterFactory

--- MoreOOP/src/main/java/lotr/CharacterFactory.java
+++ MoreOOP/src/main/java/lotr/CharacterFactory.java
@@ -2,19 +2,22 @@ package lotr;
 
 import java.util.Random;
 
-public class CharacterFactory {
-    public static Character CreateCharacter() {
-        Random random = new Random();
-        int rand = random.nextInt(101);
-        
-        if (rand <= 30) {
-            return new Elf();
-        } else if (rand <= 80) {
-            return new Knight();
-        } else if (rand <= 99) {
-            return new King();
-        } else {
-            return new Hobbit();
+class CharacterFactory {
+    private Class<? extends Character>[] characterClasses;
+    private Random random;
+
+    public CharacterFactory() {
+        characterClasses = new Class[] {Hobbit.class, Elf.class, King.class, Knight.class};
+        random = new Random();
+    }
+
+    public Character createCharacter() {
+        try {
+            int index = random.nextInt(characterClasses.length);
+            return characterClasses[index].getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
